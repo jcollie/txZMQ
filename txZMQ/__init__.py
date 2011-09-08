0cc8a77579bc042cf0f233f31f0b3d652ef62211
Jeffrey C. Ollie <jeff@ocjtech.us>
2011-09-08 14:43:24 -0500

    Add missing ZmqXREPConnection

--- txZMQ/__init__.py
+++ txZMQ/__init__.py
@@ -5,7 +5,7 @@ ZeroMQ integration into Twisted reactor.
 from txZMQ.factory import ZmqFactory
 from txZMQ.connection import ZmqEndpointType, ZmqEndpoint, ZmqConnection
 from txZMQ.pubsub import ZmqPubConnection, ZmqSubConnection
-from txZMQ.xreq_xrep import ZmqXREQConnection
+from txZMQ.xreq_xrep import ZmqXREQConnection, ZmqXREPConnection
 
 
 __all__ = ['ZmqFactory', 'ZmqEndpointType', 'ZmqEndpoint', 'ZmqConnection', 'ZmqPubConnection', 'ZmqSubConnection', 'ZmqXREPConnection', 'ZmqXREQConnection']
